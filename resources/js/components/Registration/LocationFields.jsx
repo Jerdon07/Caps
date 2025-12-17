06cfe8e9a94effd90aaa58f52b578edf83b0fa8d
Jerdon Litaoen <jdlitaoen@gmail.com>
2025-12-17 09:11:45 +0800

    feat: Implement multi-step registration form with personal info, location, and crop selection

--- resources/js/components/Registration/LocationFields.jsx
+++ resources/js/components/Registration/LocationFields.jsx
@@ -1,53 +1,168 @@
-import InputError from '@/Components/InputError';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
-export default function LocationFields({ 
-    data, 
-    municipalities, 
-    barangays, 
-    onMunicipalityChange, 
-    onBarangayChange,
-    errors 
-}) {
+import MapDialog from '@/components/Registration/MapDialog';
+import { Field, FieldLabel } from '@/components/ui/field';
+import { Select, SelectTrigger, SelectValue, SelectContent, SelectGroup, SelectLabel, SelectItem } from '@/components/ui/select';
+import { Drawer, DrawerTrigger, DrawerContent, DrawerHeader, DrawerTitle, DrawerDescription, DrawerFooter, DrawerClose } from '@/components/ui/drawer';
+import { Spinner } from '@/components/ui/spinner';
+
+import InputError from '@/components/InputError';
+import { Button } from '@/components/ui/button';
+import { MapPinned } from 'lucide-react';
+
+export default function Location({ data, setData, errors, municipalities, }) {
+    /* Address */
+    const [barangays, setBarangays] = useState([]);
+    const [loadingBarangays, setLoadingBarangays] = useState(false);
+    /* Map */
+    const [mapCenter, setMapCenter] = useState([16.4484, 120.5905]);
+    const [mapZoom, setMapZoom] = useState(10);
+    const [markerPosition, setMarkerPosition] = useState(null);
+    const [loadingCoordinates, setLoadingCoordinates] = useState(false);
+
+    /* Fetches Barangay by Municipality */
+
+    useEffect(() => {
+        if (!data.municipality_id) {
+            setBarangays([]);
+            setData('barangay_id', '');
+            setMapCenter([16.4484, 120.5905]);
+            setMapZoom(10);
+            return;
+        }
+        setLoadingBarangays(true);
+        setLoadingCoordinates(true);
+
+        axios
+            .get(`/api/barangays?municipality_id=${data.municipality_id}`)
+            .then((response) => {
+                setBarangays(response.data);
+            })
+            .finally(() => setLoadingBarangays(false));
+        
+        axios
+            .get(`/api/municipalities/${data.municipality_id}`)
+            .then((response) => {
+                const { latitude, longitude } = response.data;
+                setMapCenter([latitude, longitude]);
+                setMapZoom(12);
+            })
+            .finally(() => setLoadingCoordinates(false));
+    }, [data.municipality_id]);
+
+    const handleMapClick = (latitude, longitude) => {
+        setMarkerPosition([latitude, longitude]);
+    }
+
+    const saveData = () => {
+        if (!markerPosition) return;
+        const [lat, lng] = markerPosition;
+        setData('latitude', lat);
+        setData('longitude', lng);
+    }
+
     return (
-        <div className="space-y-4">
-            <h3 className="text-sm font-medium text-gray-900">Municipality/Barangay</h3>
-            
-            {/* Municipality */}
-            <div>
-                <select
-                    value={data.municipality_id}
-                    onChange={(e) => onMunicipalityChange(e.target.value)}
-                    className="w-full px-4 py-3 rounded-lg border border-gray-300 focus:ring-2 focus:ring-gray-900 focus:border-transparent transition-all appearance-none bg-white"
-                    required
-                >
-                    <option value="">Select Municipality</option>
-                    {municipalities.map((municipality) => (
-                        <option key={municipality.id} value={municipality.id}>
-                            {municipality.name}
-                        </option>
-                    ))}
-                </select>
-                <InputError message={errors.municipality_id} className="mt-2" />
+        <>
+            <div className="flex-0 flex flex-col items-center gap-2 text-center">
+                <h1 className="inline-flex items-center justify-center md:text-2xl text-lg font-bold gap-1"><MapPinned/>Set your Farm Address!</h1>
             </div>
 
-            {/* Barangay */}
-            <div>
-                <select
-                    value={data.barangay_id}
-                    onChange={(e) => onBarangayChange(e.target.value)}
-                    className="w-full px-4 py-3 rounded-lg border border-gray-300 focus:ring-2 focus:ring-gray-900 focus:border-transparent transition-all appearance-none bg-white"
-                    required
-                    disabled={!data.municipality_id}
+            <Field>
+                <FieldLabel htmlFor="municipality">Municipality</FieldLabel>
+                <Select value={data.municipality_id}
+                    onValueChange={(value) => {
+                        setData('municipality_id', value);
+                        setData('barangay_id', '');
+                    }}
                 >
-                    <option value="">Select Barangay</option>
-                    {barangays.map((barangay) => (
-                        <option key={barangay.id} value={barangay.id}>
-                            {barangay.name}
-                        </option>
-                    ))}
-                </select>
-                <InputError message={errors.barangay_id} className="mt-2" />
-            </div>
-        </div>
+                    <SelectTrigger className='w-[280px]'>
+                        <SelectValue placeholder="Select your Municipality" />
+                    </SelectTrigger>
+
+                    <SelectContent>
+                        <SelectGroup>
+                            <SelectLabel>Benguet Municipalities</SelectLabel>
+                            {municipalities?.map((m) => (
+                                <SelectItem key={m.id} value={String(m.id)}>
+                                    {m.name}
+                                </SelectItem>
+                            ))}
+                        </SelectGroup>
+                    </SelectContent>
+                </Select>
+                <div className='h-[16px]'>
+                    <InputError message={errors.municipality_id} className="mt-2 text-right text-xs" />
+                </div>
+            </Field>
+
+            <Field>
+                <FieldLabel htmlFor="barangay">Barangay</FieldLabel>
+                <Select value={data.barangay_id} required disabled={!data.municipality_id || loadingBarangays}
+                    onValueChange={(value) => setData('barangay_id', value)}
+                >
+                    <SelectTrigger className='w-[280px]'>
+                        <SelectValue placeholder={loadingBarangays ? <Spinner/> : 'Select Barangay'}/>
+                    </SelectTrigger>
+
+                    <SelectContent>
+                        <SelectGroup>
+                            <SelectLabel>Barangays</SelectLabel>
+                            {barangays?.map((b) => (
+                                <SelectItem key={b.id} value={String(b.id)}>
+                                    {b.name}
+                                </SelectItem>
+                            ))}
+                        </SelectGroup>
+                    </SelectContent>
+                </Select>
+                <div className='h-[16px]'>
+                    <InputError message={errors.barangay_id} className="mt-2 text-right text-xs" />
+                </div>
+            </Field>
+
+            <Field className='flex-1'>
+                <FieldLabel htmlFor='button'>Geolocation</FieldLabel>
+                <Drawer>
+                    <DrawerTrigger asChild disabled={!data.barangay_id || loadingCoordinates}>
+                        <Button variant='secondary'>{loadingCoordinates ? 'Loading Geolocation' : 'Set Geolocation'}</Button>
+                    </DrawerTrigger>
+
+                    <DrawerContent>
+                        <div className=' mx-auto w-full max-w-md gap-7'>
+                            <DrawerHeader className='gap-4'>
+                                <DrawerTitle>Set Geolocation</DrawerTitle>
+                                <DrawerDescription>
+                                    Set your farm&apos;s geolocation by selecting it on the map.
+                                </DrawerDescription>
+                            </DrawerHeader>
+
+                            <div
+                                onPointerDown={(e) => e.stopPropagation()}
+                                onPointerMove={(e) => e.stopPropagation()}
+                                onPointerUp={(e) => e.stopPropagation()}
+                            >
+                                <MapDialog
+                                center={mapCenter}
+                                zoom={mapZoom}
+                                onSelect={handleMapClick}
+                                markerPosition={markerPosition}
+                            />
+                            </div>
+
+                            <DrawerFooter>
+                                <DrawerClose asChild>
+                                    <Button variant='outline'>Cancel</Button>
+                                </DrawerClose>
+                                <DrawerClose asChild>
+                                    <Button onClick={saveData}>Save</Button>
+                                </DrawerClose>
+                            </DrawerFooter>
+                        </div>
+                    </DrawerContent>
+                </Drawer>
+                <InputError message={[errors.latitude, errors.longitude]} className="mt-2" />
+            </Field>
+        </>
     );
 }
--- resources/js/components/Registration/PersonalInfoFields.jsx
+++ resources/js/components/Registration/PersonalInfoFields.jsx
@@ -1,114 +1,125 @@
-import InputError from '@/Components/InputError';
-import { Eye, EyeOff } from 'lucide-react';
 
-export default function PersonalInfoFields({ data, setData, errors, showPassword, setShowPassword, showPasswordConfirmation, setShowPasswordConfirmation }) {
+import { useState } from 'react';
+import { Eye, EyeOff, User } from 'lucide-react';
+
+import { Field, FieldLabel, FieldDescription } from '@/components/ui/field';
+import { Input } from '@/components/ui/input';
+import InputError from '@/components/InputError';
+
+export default function Personal({ data, setData, errors }) {
+    const [showPassword, setShowPassword] = useState(false);
+    const [showConfPassword, setShowConfPassword] = useState(false);
+
     return (
         <>
-            {/* Name Field */}
-            <div>
-                <label htmlFor="name" className="block text-sm font-medium text-gray-700 mb-2">
-                    Name
-                </label>
-                <input
+            <div className="flex flex-col items-center gap-2 text-center">
+                <h1 className="inline-flex items-center md:text-2xl justify-center text-lg font-bold gap-1"><User size={25}/>Account Registration</h1>
+            </div>
+
+            <Field className='gap-1'>
+                <FieldLabel htmlFor='name'>Full Name</FieldLabel>
+                <Input
                     id="name"
                     name="name"
                     type="text"
                     value={data.name}
                     onChange={(e) => setData('name', e.target.value)}
-                    placeholder="Jane Doe"
-                    className="w-full px-4 py-3 rounded-lg border border-gray-300 focus:ring-2 focus:ring-gray-900 focus:border-transparent transition-all"
+                    placeholder="Juan Doe"
                     required
                     autoFocus
                 />
-                <InputError message={errors.name} className="mt-2" />
-            </div>
-
-            {/* Email Field */}
-            <div>
-                <label htmlFor="email" className="block text-sm font-medium text-gray-700 mb-2">
-                    Email Address
-                </label>
-                <input
+                <div className='h-[16px]'>
+                    {errors.name && (
+                        <InputError message={errors.name} className="mt-2 text-right text-xs"/>
+                    )}
+                </div>
+            </Field>
+        
+            <Field className='gap-1'>
+                <FieldLabel htmlFor="email">Email Address</FieldLabel>
+                <Input
                     id="email"
                     name="email"
                     type="email"
                     value={data.email}
                     onChange={(e) => setData('email', e.target.value)}
                     placeholder="name@email.com"
-                    className="w-full px-4 py-3 rounded-lg border border-gray-300 focus:ring-2 focus:ring-gray-900 focus:border-transparent transition-all"
                     required
                 />
-                <InputError message={errors.email} className="mt-2" />
-            </div>
-
-            {/* Password Field */}
-            <div>
-                <label htmlFor="password" className="block text-sm font-medium text-gray-700 mb-2">
-                    Password
-                </label>
-                <div className="relative">
-                    <input
-                        id="password"
-                        name="password"
-                        type={showPassword ? "text" : "password"}
-                        value={data.password}
-                        onChange={(e) => setData('password', e.target.value)}
-                        placeholder="Create a password"
-                        className="w-full px-4 py-3 rounded-lg border border-gray-300 focus:ring-2 focus:ring-gray-900 focus:border-transparent transition-all pr-10"
-                        required
-                    />
-                    <button
-                        type="button"
-                        onClick={() => setShowPassword(!showPassword)}
-                        className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-400 hover:text-gray-600"
-                    >
-                        {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
-                    </button>
+                <div className='h-[16px]'>
+                    <InputError message={errors.email} className="mt-2 text-right text-xs"/>
                 </div>
-                <InputError message={errors.password} className="mt-2" />
-            </div>
+            </Field>
 
-            {/* Confirm Password Field */}
-            <div>
-                <div className="relative">
-                    <input
-                        id="password_confirmation"
-                        name="password_confirmation"
-                        type={showPasswordConfirmation ? "text" : "password"}
-                        value={data.password_confirmation}
-                        onChange={(e) => setData('password_confirmation', e.target.value)}
-                        placeholder="Confirm your Password"
-                        className="w-full px-4 py-3 rounded-lg border border-gray-300 focus:ring-2 focus:ring-gray-900 focus:border-transparent transition-all pr-10"
-                        required
-                    />
-                    <button
-                        type="button"
-                        onClick={() => setShowPasswordConfirmation(!showPasswordConfirmation)}
-                        className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-400 hover:text-gray-600"
-                    >
-                        {showPasswordConfirmation ? <EyeOff size={20} /> : <Eye size={20} />}
-                    </button>
+            <Field className='gap-1'>
+                <Field className='grid grid-cols-2 gap-4'>
+                    <Field className='gap-1'>
+                        <FieldLabel htmlFor="password">Password</FieldLabel>
+                        <div className='relative'>
+                            <Input
+                                id="password"
+                                name="password"
+                                type={showPassword ? "text" : "password"}
+                                value={data.password}
+                                onChange={(e) => setData('password', e.target.value)}
+                                placeholder="********"
+                                required
+                            />
+                            <button
+                                type="button"
+                                onClick={() => setShowPassword(!showPassword)}
+                                className='absolute right-3 top-1/2 -translate-y-1/2 text-gray-400 hover:text-gray-600'
+                            >
+                                {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+                            </button>
+                        </div>
+                    </Field>
+                    <Field className='gap-1'>
+                        <FieldLabel htmlFor='password'>Confirm Password</FieldLabel>
+
+                        <div className='relative'>
+                            <Input
+                                id="password_confirmation"
+                                name="password_confirmation"
+                                type={showConfPassword ? "text" : "password"}
+                                value={data.password_confirmation}
+                                onChange={(e) => setData('password_confirmation', e.target.value)}
+                                placeholder="********"
+                                required
+                            />
+
+                            <button
+                                type="button"
+                                onClick={() => setShowConfPassword(!showConfPassword)}
+                                className='absolute right-3 top-1/2 -translate-y-1/2 text-gray-400 hover:text-gray-600'
+                            >
+                                {showConfPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+                            </button>
+                        </div>
+                        
+                    </Field>
+                </Field>
+                <div className='h-[16px]'>
+                    <InputError message={errors.password} className="mt-2 text-right text-xs"/>
+                    <InputError message={errors.password_confirmation} className="mt-2 text-right text-xs"/>
                 </div>
-                <InputError message={errors.password_confirmation} className="mt-2" />
-            </div>
+                
 
-            {/* Phone Number Field */}
-            <div>
-                <label htmlFor="phone_number" className="block text-sm font-medium text-gray-700 mb-2">
-                    Phone Number
-                </label>
-                <input
+                <FieldDescription  className='text-xs'>Must be at least 8 characters long.</FieldDescription>
+            </Field>
+            <Field className='gap-1 flex-1'>
+                <FieldLabel htmlFor="phone_number">Phone Number</FieldLabel>
+                <Input
                     id="phone_number"
                     name="phone_number"
                     type="tel"
                     value={data.phone_number}
                     onChange={(e) => setData('phone_number', e.target.value)}
                     placeholder="+63 912 345 6789"
-                    className="w-full px-4 py-3 rounded-lg border border-gray-300 focus:ring-2 focus:ring-gray-900 focus:border-transparent transition-all"
                     required
                 />
-                <InputError message={errors.phone_number} className="mt-2" />
-            </div>
+                <InputError message={errors.phone_number} className="mt-2 text-right text-xs"/>
+            </Field>
         </>
     );
 }
