3d1edc8a8b9041710b6eb72a717bae23e7bdf9ed
Jerdon07 <jdlitaoen@gmail.com>
2025-12-26 22:22:27 +0800

    refactor: temporary Remove the crop seeds

--- database/seeders/CropSeeder.php
+++ database/seeders/CropSeeder.php
@@ -12,18 +12,6 @@ class CropSeeder extends Seeder
      */
     public function run(): void
     {
-        $crops = [
-            ['category_id' => 1, 'name' => 'Carrot', 'price_min' => 40, 'price_max' => 80, 'crop_weeks' => 10],
-            ['category_id' => 1, 'name' => 'Cabbage', 'price_min' => 30, 'price_max' => 60, 'crop_weeks' => 12],
-            ['category_id' => 2, 'name' => 'Banana', 'price_min' => 50, 'price_max' => 100, 'crop_weeks' => 52],
-        ];
-
-        foreach ($crops as $crop) {
-            Crop::FirstOrCreate(
-                ['category_id' => $crop['category_id'], 'name' => $crop['name']],
-                $crop
-            );
-        }
-
+        
     }
 }
