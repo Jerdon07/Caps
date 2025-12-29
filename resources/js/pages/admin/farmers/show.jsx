69b85486fcb005650973578dfc56ea12bdd7e1d6
Jerdon07 <jdlitaoen@gmail.com>
2025-12-29 20:41:00 +0800

    feat: Create page for showing a user

---
+++ resources/js/pages/admin/farmers/show.jsx
@@ -0,0 +1,11 @@
+import AdminLayout from "@/layouts/admin-layout";
+
+export default function Show({ farmer }) {
+    console.log(farmer)
+
+    return (
+        <AdminLayout>
+            Show user
+        </AdminLayout>
+    )
+}
