38b8676ae44e0ac37748e4da4920332ebe016a5d
Jerdon07 <jdlitaoen@gmail.com>
2026-01-03 23:21:40 +0800

    refactor: Apply the transformed layer name from CropResource

--- resources/js/pages/Crops/Index.jsx
+++ resources/js/pages/Crops/Index.jsx
@@ -7,13 +7,14 @@ import { AspectRatio } from '@/components/ui/aspect-ratio';
 import { Separator } from '@/components/ui/separator';
 
 import { ImageOff } from 'lucide-react';
+import { Button } from '@/components/ui/button';
 
 const Crops = ({ crops, categories, filters }) => {
     const [selectedCategory, setSelectedCategory] = useState(null);
     const filteredCrops = selectedCategory
         ? crops.filter(crop => crop.category_id === selectedCategory)
         : crops;
-
+    console.log(crops)
     return (
         <AppLayout 
             title='Crops'
@@ -31,13 +32,24 @@ const Crops = ({ crops, categories, filters }) => {
                 <div className="grid auto-rows-min gap-4 md:grid-cols-5">
                     {filteredCrops.map(crop => (
                         <Card key={crop.id} className='p-0 max-w-sm cursor-pointer transition-all duration-200 hover:shadow-xl hover:-translate-y-1 hover:border-primary'>
-                            <CardContent onClick={() => {console.log('clickable')}} className='w-full p-0 rounded-t-xl overflow-hidden'>
-                                <AspectRatio ratio={16/9} className='h-full'>
-                                    {crop.image_path ? (
-                                        <img src={`/storage/${crop.image_path}`} alt={crop.name} className="w-full h-full rounded-t-xl" />
+                            <CardContent onClick={() => console.log('Go to Price Insights')} className='w-full p-0 rounded-t-xl overflow-hidden'>
+                                <AspectRatio ratio={16/9} className='h-full relative'>
+                                    {crop.imagePath ? (
+                                        <img
+                                            src={crop.imagePath.startsWith('http')
+                                                ? crop.imagePath
+                                                : `/storage/${crop.imagePath}`}
+                                            alt={crop.name}
+                                            className="w-full h-full rounded-t-xl" />
                                     ) : (
                                         <div className="w-full h-full flex items-center justify-center bg-primary text-card"><ImageOff size={40} /></div>
                                     )}
+                                    <Button
+                                        size='sm'
+                                        className='absolute top-2 right-2'
+                                    >
+                                        {crop.latestPrice.recordedAt}
+                                    </Button>
                                 </AspectRatio>
                                 
                                 <CardHeader className='py-1 px-2 gap-1'>
@@ -45,10 +57,10 @@ const Crops = ({ crops, categories, filters }) => {
                                     <Separator />
                                     <CardDescription className='flex flex-col text-xs'>
                                         <div>
-                                            <p>₱: {parseFloat(crop.price_min).toFixed(2)}-{parseFloat(crop.price_max).toFixed(2)}</p>
+                                            <p>{parseFloat(crop.latestPrice.priceMin).toFixed(2)} - {parseFloat(crop.latestPrice.priceMax).toFixed(2)} PHP</p>
                                         </div>
                                         <div className=''>
-                                            <p className='text-right'>{crop.crop_weeks} average weeks</p>
+                                            <p className='text-right'>{crop.cropWeeks} average weeks</p>
                                         </div>
                                         
                                     </CardDescription>
